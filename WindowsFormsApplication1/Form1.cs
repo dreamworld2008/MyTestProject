c3d191e9ab51cf5d2af16da3f077b70dc251a5f4
dreamworld2008 <2459163101@qq.com>
2014-08-07 13:44:26 +0800

    Add tip

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("hello,world!");
+            MessageBox.Show("hello,world!");//Show Windows Box
         }
     }
 }
